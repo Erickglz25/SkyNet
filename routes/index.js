60e6b04cda3c1191ae7ef4a7ad88c486158970c5
Erickglz25 <Erick.glz25@gmail.com>
2016-11-18 12:55:55 -0600

    AddRegisterPage

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SkyNet',condition:true});
 });
 
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'SkyNet',condition:true});
+});
+
+
+
+
 module.exports = router;
